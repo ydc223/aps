e7aa17cdb7cd3c535afe73954f77175018c1bf5b
robwayne <robwaynegordon@gmail.com>
2018-12-09 17:56:00 -0500

    added another test case and ans

--- piratesteakhouse/gen/gen.py
+++ piratesteakhouse/gen/gen.py
@@ -1,10 +1,13 @@
-def largest_time():
+def largest_time(delay):
   MAXN = int(1e5)
   res = "%d %d\n"%(MAXN, MAXN-1)
   for i in range(1,MAXN):
-    res += '%d %d %d\n'%(i,i+1, MAXN-i)
+    if (delay == -1):
+      res += '%d %d %d\n'%(i,i+1, -1)
+    else:
+      res += '%d %d %d\n'%(i,i+1, MAXN-i)
   
-  k, t = MAXN-1, 1
+  k, t = MAXN-1, 1e9
   res += "%d %d\n"%(k, t)
   for i in range(2,MAXN+1):
     res += str(i)+" "
@@ -13,5 +16,13 @@ def largest_time():
   res += "\n"
   return res
 
-with open('../data/secret/004-secret4.in', 'w+') as fp:
-  fp.write(largest_time())
+def gen_largest():
+  with open('../data/secret/004-secret4.in', 'w+') as fp:
+    fp.write(largest_time(0))
+
+def gen_largest_delay():
+  with open("../data/secret/006-secret6.in", "w+") as fp:
+    fp.write(largest_time(-1))
+
+gen_largest()
+gen_largest_delay()
