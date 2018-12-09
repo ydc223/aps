4d8d6138bdde2ffd6e47c5ee4c9a6b1867cc2c4c
robwayne <robwaynegordon@gmail.com>
2018-12-09 17:03:46 -0500

    generator script

--- skeleton/gen/gen.py
+++ skeleton/gen/gen.py
@@ -1,6 +1,6 @@
 def largest_time():
   MAXN = int(10e5)
-  res = ""
+  res = "%d %d\n"%(MAXN, MAXN-1)
   for i in range(1,MAXN):
     res += '%d %d %d\n'%(i,i+1, MAXN-i)
   
@@ -8,7 +8,6 @@ def largest_time():
   res += "%d %d\n"%(k, t)
   for i in range(2,k+1):
     res += str(i)+" "
-  res += "\n"
   return res
 
 with open('../data/secret/004-secret4.in', 'w+') as fp:
