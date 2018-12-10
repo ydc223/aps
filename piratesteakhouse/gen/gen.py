2d3dddffd0e9b53a991faf2f48e2b4e545b8006f
robwayne <robwaynegordon@gmail.com>
2018-12-09 22:29:09 -0500

    created random connected graph input

--- piratesteakhouse/gen/gen.py
+++ piratesteakhouse/gen/gen.py
@@ -1,3 +1,5 @@
+import random
+
 MAXN = int(1e5)
 def largest_time(delay, time):
   res = "%d %d\n"%(MAXN, MAXN-1)
@@ -25,11 +27,48 @@ def gen_largest_delay():
     fp.write(largest_time(-1, 1e9))
 
 
-def random_cases():
-  
+def random_cases_connected():
+  n = random.randint(2e4, MAXN+1)
+  all_vertices = set(i for i in range(2, n+1))
+  used_vertices = set()
+  random_vertex = 1
+  res = "" 
+  m = 0
+  while len(all_vertices) > 0:
+    end_vertex = random.sample(all_vertices, 1)[0]
+    if end_vertex in used_vertices or end_vertex == random_vertex: 
+      continue
+    m += 1
+    if m > 2*MAXN: break
+    rand_delay = random.randint(1,1e9)
+    rand_chance = random.random()
+    if rand_chance < 0.5:
+      rand_delay = -1
+    res += "%d %d %d\n"%(random_vertex, end_vertex, rand_delay)
+    used_vertices.add(end_vertex)
+    all_vertices.remove(end_vertex)
+    random_vertex = end_vertex
 
+  output = "%d %d\n"%(n, m)
+  output += res
+
+  k = random.randint(1,n)
+  t = random.randint(1,1e9+1)
+  output += "%d %d\n"%(k, t)
+  vertices = list(used_vertices)
+  for i in random.sample(vertices, k):
+    output += "%d "%i
+  output.strip()
+  output += "\n"
+  return output
+
+
+def gen_random_cases():
+  with open('../data/secret/007-secret7.in', 'w+') as fp:
+    fp.write(random_cases_connected())
 
 gen_largest()
 gen_largest_delay()
+gen_random_cases()
 
 
